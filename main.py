5e03fc0a2adb29ac568eff222dc1c1145b460f0a
Роман Галушкин <galushkinrr@gmail.com>
2023-09-04 06:12:02 +0700

    1v

---
+++ main.py
@@ -0,0 +1,25 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config_reader import config
+
+from handlers import questions, different_types
+
+
+logging.basicConfig(level=logging.INFO)
+
+async def main():
+    bot = Bot(config.bot_token.get_secret_value())
+    dp = Dispatcher()
+
+    dp.include_router(questions.router)
+    dp.include_router(different_types.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+
+if __name__=='__main__':
+    asyncio.run(main())
